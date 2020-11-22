f42bc6e6dcef4d2a81d4d6b01dc6d3c3eb482047
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-11-23 00:06:25 +0200

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,6 @@
 
 * Update docs
 
-## [0.1.0] - 2020-11-23.
+## [0.2.0] - 2020-11-23.
 
 * Add credit card validators
--- lib/form_validators.dart
+++ lib/form_validators.dart
@@ -1,50 +1,5 @@
 library form_validators;
 
-/// An interface used to create a validator class
-/// you can also used to create your own validation class
-/// ```
-/// class CustomClass implements IValidator {
-///   final String message = 'Message to be used';
-///
-///   you can mark the message prop as optional with [this.message]
-///   CustomClass(this.message) {
-///
-///   }
-///
-///   call(String value) {
-///     should return true;
-///     return ("if meets my condition") ? false : true;
-///     this means if every thing is good return false, no message needed to display otherwise return true to show the message
-///   }
-///
-/// }
-/// ```
-class IValidator {
-  final String message;
-  IValidator(this.message);
-  call(String value) => bool;
-}
-
-/// Used to run the validators in sequence
-/// this function must be used in validate function
-/// ```
-/// TextFormField(
-///   validator: validate([
-///     validators.Required('This field is required'),
-///     validators.Email('Please enter the email correctly'),
-/// ]))
-/// ```
-validate(List<IValidator> validators) {
-  return (String value) {
-    for (var validator in validators) {
-      if (validator(value)) {
-        return validator.message;
-      }
-    }
-    return null;
-  };
-}
-
 /// Check if the field value contain a certain string
 class Contains implements IValidator {
   final String seed;
@@ -156,116 +111,47 @@ class Pattern implements IValidator {
   Pattern(String regexb, [this.message = '']) : this.regexp = RegExp(regexb);
 }
 
-class CreditCardMonthValidator implements IValidator {
-  /// A message that will be used as default if non if other messages are provided
-  String defaultMessage;
-  String message;
-
-  /// error message that will be shown in case of date expiration or format
-  String dateInvalidMessage;
-  String yearInvalidMessage;
-  String monthInvalidMessage;
-  CreditCardMonthValidator({
-    this.defaultMessage,
-    this.dateInvalidMessage,
-    this.yearInvalidMessage,
-    this.monthInvalidMessage,
-  });
-
-  int fullYear(int year) {
-    if (year < 100 && year >= 0) {
-      var now = DateTime.now();
-      String currentYear = now.year.toString();
-      String prefix = currentYear.substring(0, currentYear.length - 2);
-      year = int.parse('$prefix${year.toString().padLeft(2, '0')}');
-    }
-    return year;
-  }
-
-  bool expired(int month, int year) {
-    var now = DateTime.now();
-    int fourDigitsYear = fullYear(year);
-    if (fourDigitsYear < now.year && month != now.month) {
-      return true;
-    }
-    return false;
-  }
-
-  bool call(String value) {
-    int year;
-    int month;
-
-    try {
-      var split = value.split(RegExp(r'(\/)'));
-      month = int.parse(split[0]);
-      year = int.parse(split[1]);
-    } catch (e) {
-      this.message = this.dateInvalidMessage ?? this.defaultMessage ?? 'Date is invalid';
-      return true;
-    }
-
-    if ((month < 1) || (month > 12)) {
-      this.message = this.monthInvalidMessage ?? this.defaultMessage ?? 'Month is invalid';
-      return true;
-    }
-
-    var fourDigitsYear = fullYear(year);
-    if ((fourDigitsYear < 1) || (fourDigitsYear > 2099)) {
-      this.message = this.yearInvalidMessage ?? this.defaultMessage ?? 'Year is invalid';
-
-      return true;
-    }
-
-    if (!expired(month, year)) {
-      this.message = this.dateInvalidMessage ?? this.defaultMessage ?? 'Date is invalid';
-      return true;
-    }
-    return false;
-  }
+/// An interface used to create a validator class
+/// you can also used to create your own validation class
+/// ```
+/// class CustomClass implements IValidator {
+///   final String message = 'Message to be used';
+///
+///   you can mark the message prop as optional with [this.message]
+///   CustomClass(this.message) {
+///
+///   }
+///
+///   call(String value) {
+///     should return true;
+///     return ("if meets my condition") ? false : true;
+///     this means if every thing is good return false, no message needed to display otherwise return true to show the message
+///   }
+///
+/// }
+/// ```
+class IValidator {
+  final String message;
+  IValidator(this.message);
+  call(String value) => bool;
 }
 
-class CreditCardNumberValidator implements IValidator {
-  final String message;
-
-  CreditCardNumberValidator([this.message = "Number is not valid"]);
-
-  bool call(String value) {
-    final formattedValue = keepNumbersOnly(value);
-    return !(Pattern(r"[^0-9]").call(formattedValue) && luhnCheck(formattedValue));
-  }
-
-  String keepNumbersOnly(String value) {
-    RegExp regExp = RegExp(r"[^0-9]");
-    return value.replaceAll(regExp, '');
-  }
-
-  bool luhnCheck(String number) {
-    var sum = int.parse(number[number.length - 1]);
-
-    for (var i = 0; i < number.length - 1; i++) {
-      var value = int.parse(number[i]);
-
-      if (i % 2 == 0) {
-        value *= 2;
+/// Used to run the validators in sequence
+/// this function must be used in validate function
+/// ```
+/// TextFormField(
+///   validator: validate([
+///     validators.Required('This field is required'),
+///     validators.Email('Please enter the email correctly'),
+/// ]))
+/// ```
+validate(List<IValidator> validators) {
+  return (String value) {
+    for (var validator in validators) {
+      if (validator(value)) {
+        return validator.message;
       }
-
-      if (value > 9) {
-        value -= 9;
-      }
-
-      sum += value;
     }
-
-    return sum % 10 == 0;
-  }
-}
-
-class CreditCardCVCValidator implements IValidator {
-  final String message;
-  CreditCardCVCValidator([this.message = "Number is not valid"]);
-
-  @override
-  bool call(String value) {
-    return value.length < 3 || value.length > 4;
-  }
+    return null;
+  };
 }
